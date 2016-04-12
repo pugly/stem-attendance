5bd4cbfacf16c8cc74eda2dbad735142b5f6b5e7
pugly <bonezep3@gmail.com>
2016-04-12 09:53:44 -0400

    End Infinite Loop
    Creates option to exit loop by inputting Q or q

--- attendance.py
+++ attendance.py
@@ -18,3 +18,6 @@ student_info = {'4401001': 'Christine Adewale',
 while True:
     s_id = input('Scan student id: ')
     print('%s checked in - %s' % (student_info[s_id], time.time()))
+    q = input('To quit type Q or q: ')
+    if 'Q' or 'q' in q:
+        break
